9353608b7d821c65e899f4498c5342a59a461a67
Your Name <you@example.com>
2023-05-18 13:22:01 +0300

    changed i from 10 to 20

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class GitTest {
     public static void main(String[] args) {
-        for(int i = 0; i <= 10; i++){
+        for(int i = 0; i <= 20; i++){
           if(i == 5) { // to confess love to Java and Git hub
               System.out.println(i + " I like Java and GitHub");
           }
