cde4b355a05b5ab149b689cbe20037867f5d47ae
Anastasiya Vusik <133912882+NastyaVusik@users.noreply.github.com>
2023-05-18 13:13:24 +0300

    Add files via upload

---
+++ src/GitTest.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class GitTest {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 10; i++){
+          if(i == 5) { // to confess love to Java and Git hub
+              System.out.println(i + " I like Java and GitHub");
+          }
+          else {
+              System.out.println(i + " Git is easy"); // to convince myself that Git is easy
+          }
+
+        }
+    }
+}
